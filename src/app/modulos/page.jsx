ec30f3eda4383dce2bf24aa8694feb5916063685
Alex Fonseca <104625458+sousaalex@users.noreply.github.com>
2024-07-03 19:13:46 +0100

    Update page.jsx

--- src/app/modulos/page.jsx
+++ src/app/modulos/page.jsx
@@ -109,7 +109,7 @@ const TypingAnimationHeader = ({ text }) => {
 
 // Função para remover caracteres indesejados
 const cleanText = (text) => {
-  return text.replace(/[.*+#]|[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|(\bhttps?:\/\/\S+\b)/gu, '');
+  return text.replace(/[*+#]|[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|(\bhttps?:\/\/\S+\b)/gu, '');
 };
 
 // Função para converter texto em fala
