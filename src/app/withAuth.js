c26f916236a1ac5b8b912b0aa34626c55881188e
Alex Fonseca <104625458+sousaalex@users.noreply.github.com>
2024-07-05 19:26:37 +0100

    Update withAuth.js

--- src/app/withAuth.js
+++ src/app/withAuth.js
@@ -64,7 +64,7 @@ class WithAuth extends React.Component {
 
     if (isChecking) {
       return (
-        <div className="w-full h-full fixed top-0 left-0 bg-black bg-opacity-50 flex justify-center items-center">
+        <div className="w-full h-full fixed top-0 left-0  flex justify-center items-center">
           <div className="flex justify-center items-center text-center space-x-2">
             <Spin size="small" />
           </div>
