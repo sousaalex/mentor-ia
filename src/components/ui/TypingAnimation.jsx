57ff8fe124aeaf3d2af48240525c829a47eeab2a
Alex Fonseca <104625458+sousaalex@users.noreply.github.com>
2024-07-08 18:46:57 +0100

    Update TypingAnimation.jsx

--- src/components/ui/TypingAnimation.jsx
+++ src/components/ui/TypingAnimation.jsx
@@ -1,6 +1,5 @@
 'use client'
 import { useEffect, useState, useRef } from 'react';
-import { marked } from 'marked';
 
 export default function TypingAnimation({ text }) {
   const [content, setContent] = useState('');
@@ -12,14 +11,14 @@ export default function TypingAnimation({ text }) {
     const animateText = async () => {
       for (let i = 0; i < text.length; i++) {
         if (!isMounted) return;
-        await new Promise((resolve) => setTimeout(resolve, 5)); // Ajuste o tempo para simular a digitação
+        await new Promise((resolve) => setTimeout(resolve, 50)); // Ajuste o tempo para simular a digitação
 
         if (isMounted) {
           setContent((prevContent) => prevContent + text.charAt(i));
 
           // Verificar se a API de vibração está disponível e vibrar o dispositivo móvel
           if ('vibrate' in navigator) {
-            navigator.vibrate(50); // Vibra por 50 milissegundos
+            navigator.vibrate(10); // Vibra por 10 milissegundos (ajuste conforme necessário)
           }
 
           window.requestAnimationFrame(() => {
@@ -40,26 +39,9 @@ export default function TypingAnimation({ text }) {
     <>
       <p
         className="rounded-xl text-base font-normal"
-        dangerouslySetInnerHTML={{ __html: marked(content) }}
-        style={{ animation: 'fadeIn 0.5s ease-in-out' }} // Adiciona efeito de aparição
+        dangerouslySetInnerHTML={{ __html: content }}
       />
       <div ref={chatEndRef} />
-      <style jsx>{`
-        @keyframes fadeIn {
-          from {
-            opacity: 0;
-            filter: blur(5px);
-          }
-          to {
-            opacity: 1;
-            filter: blur(0);
-          }
-        }
-
-        p {
-          animation: fadeIn 0.5s ease-in-out; // Aplica a animação de fadeIn
-        }
-      `}</style>
     </>
   );
 }
