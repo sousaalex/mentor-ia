b80dd739f20b71c5e2770505d79eb83d4c84925e
Alex Fonseca <104625458+sousaalex@users.noreply.github.com>
2024-07-08 18:35:43 +0100

    Update TypingAnimation.jsx

--- src/components/ui/TypingAnimation.jsx
+++ src/components/ui/TypingAnimation.jsx
@@ -1,11 +1,10 @@
 'use client'
-import {useEffect, useState, useRef} from 'react'
+import { useEffect, useState, useRef } from 'react';
 import { marked } from 'marked';
 
 export default function TypingAnimation({ text }) {
   const [content, setContent] = useState('');
   const chatEndRef = useRef(null);
-
 
   useEffect(() => {
     let isMounted = true;
@@ -13,9 +12,16 @@ export default function TypingAnimation({ text }) {
     const animateText = async () => {
       for (let i = 0; i < text.length; i++) {
         if (!isMounted) return;
-        await new Promise((resolve) => setTimeout(resolve, 2));
+        await new Promise((resolve) => setTimeout(resolve, 5)); // Ajuste o tempo para simular a digitação
+
         if (isMounted) {
           setContent((prevContent) => prevContent + text.charAt(i));
+
+          // Verificar se a API de vibração está disponível e vibrar o dispositivo móvel
+          if ('vibrate' in navigator) {
+            navigator.vibrate(50); // Vibra por 50 milissegundos
+          }
+
           window.requestAnimationFrame(() => {
             chatEndRef.current?.scrollIntoView({ behavior: "instant" });
           });
@@ -32,9 +38,28 @@ export default function TypingAnimation({ text }) {
 
   return (
     <>
-      <p className=" rounded-xl text-base  font-normal" dangerouslySetInnerHTML={{ __html: marked(content) }} />
+      <p
+        className="rounded-xl text-base font-normal"
+        dangerouslySetInnerHTML={{ __html: marked(content) }}
+        style={{ animation: 'fadeIn 0.5s ease-in-out' }} // Adiciona efeito de aparição
+      />
       <div ref={chatEndRef} />
+      <style jsx>{`
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
+            filter: blur(5px);
+          }
+          to {
+            opacity: 1;
+            filter: blur(0);
+          }
+        }
+
+        p {
+          animation: fadeIn 0.5s ease-in-out; // Aplica a animação de fadeIn
+        }
+      `}</style>
     </>
   );
-};
-
+}
